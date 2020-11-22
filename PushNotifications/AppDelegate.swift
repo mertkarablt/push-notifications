ce238296833b0d5f4b6e981265699b14ea337e9e
Mert Karabulut <mert.karabulut@loodos.com>
2020-11-22 15:37:08 +0300

    Request authorization for notifications

--- PushNotifications/AppDelegate.swift
+++ PushNotifications/AppDelegate.swift
@@ -6,19 +6,19 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        registerForPushNotifications()
+
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -34,3 +34,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+// MARK: - Push Notifications
+extension AppDelegate {
+    func registerForPushNotifications() {
+        let center =  UNUserNotificationCenter.current()
+        let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        center.requestAuthorization(
+            options: options) { (granted, error) in
+            if let error = error {
+                // Handle the error here.
+                print("registerForPushNotifications - Error: \(error)")
+            }
+
+            // Enable or disable features based on the authorization.
+            print("registerForPushNotifications - Granted: \(granted)")
+        }
+    }
+}
