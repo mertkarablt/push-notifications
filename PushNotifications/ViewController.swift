f5c038fffcda55cf4211676f447164982cc1736b
Mert Karabulut <mert.karabulut@loodos.com>
2020-11-22 13:20:35 +0300

    Project init

---
+++ PushNotifications/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PushNotifications
+//
+//  Created by Mert Karabulut on 22.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
